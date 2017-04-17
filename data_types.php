5a4eb8f15ef561a4b294784786f840464dc20c9d
drumdrummer <divushak_a@mail.ru>
2017-04-17 17:03:41 +0300

    tak?

--- data_types.php
+++ data_types.php
@@ -1,12 +1,23 @@
 <?php
 $boolean="true";
+echo $boolean;
 $integer="123";
+echo $integer;
 $float="double";
+echo $float;
 $string="stg";
+echo $string;
 $array="index";
+echo $array;
 $object="oop";
+echo $object;
 $resource="url";
+echo $resource;
 $null="0";
+echo $null;
 $mixed="all types";
+echo $mixed;
 $number="int or fl";
+echo $number;
 $callback="call";
+echo $callback;

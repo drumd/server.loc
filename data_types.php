48c39b93177e58ab288db6b2abc792e54444067b
drumdrummer <divushak_a@mail.ru>
2017-04-17 11:28:09 +0300

    types

---
+++ data_types.php
@@ -0,0 +1,12 @@
+<?php
+$boolean="true";
+$integer="123";
+$float="double";
+$string="stg";
+$array="index";
+$object="oop";
+$resource="url";
+$null="0";
+$mixed="all types";
+$number="int or fl";
+$callback="call";
